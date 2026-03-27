272d1acce99c52ec770bd57f240e530cb2263cfd
Chetan Sai <chetan142006@gmail.com>
2026-03-27 18:30:14 +0530

    Refactor primality test using BigInteger

--- Java/JavaPrimalityTest.java
+++ Java/JavaPrimalityTest.java
@@ -4,19 +4,7 @@ public class JavaPrimalityTest {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         String n = bufferedReader.readLine();
-        int a = Integer.parseInt(n);
-        int count=0;
-        for(int i=1;i<=a;i++){
-            if(a%i==0){
-                count++;
-            }
-        }
-        if(count==2){
-            System.out.println("prime");
-        }
-        else{
-            System.out.println("Not prime");
-        }
+        System.out.print(new BigInteger(n).isProbablePrime(100) ? "prime" : "not prime");
         bufferedReader.close();
     }
 }
